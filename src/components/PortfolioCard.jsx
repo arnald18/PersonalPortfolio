fa656e89d660b9d26c672320b5b2b5255ca1dab3
arnald18 <acenaldy04@gmail.com>
2023-09-03 23:01:05 -0400

    add PortfolioCard and PortfolioCard to PotfolioPage

---
+++ src/components/PortfolioCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function PortfolioCard({
+  image,
+  title,
+  deployedLink,
+  repoLink,
+}) {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img className="rounded-t-lg" src={image} alt={`${title} screenshot`} />
+
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+
+        <a
+          href={repoLink}
+          target="_blank"
+          rel="noreferrer"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+          Repo
+        </a>
+        <a
+          href={deployedLink}
+          target="_blank"
+          rel="noreferrer"
+          className="inline-flex ml-3 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Live Site
+          <svg
+            className="w-3.5 h-3.5 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,61 @@
+/**
+ *
+ *^ 6 portfolio apllications
+ *
+ * Image
+ * Title
+ * -Link to the  deployed site
+ * - Link to github repo
+ *
+ *
+ */
+
+import PortfolioCard from "../components/PortfolioCard";
+import ThoughtHarvesterImage from "../assets/ThoughtHarvester-screenshot.png.png";
+import SoundQuestImage from "../assets/SoundQuest-screenshot.png";
+import HoriseonImage from "../assets/Horiseon-screenshot.png";
+
 export default function Portfolio() {
   return (
     <div>
-      <div></div>
+      <div>
+        <PortfolioCard
+          image={ThoughtHarvesterImage}
+          title="ThoughtHarvester"
+          repoLink="https://github.com/arnald18/ThoughtHarvester"
+          deployedLink="https://secret-cliffs-31610-1cfd2b52332b.herokuapp.com/"
+        />
+        <PortfolioCard
+          image={HoriseonImage}
+          title="Horiseon"
+          repoLink="https://github.com/arnald18/Horiseon"
+          deployedLink="https://arnald18.github.io/Horiseon/"
+        />
+        <PortfolioCard
+          image={SoundQuestImage}
+          title="Sound Quest"
+          repoLink="https://github.com/holdenmg/sound-quest"
+          deployedLink="https://holdenmg.github.io/sound-quest/"
+        />
+        <PortfolioCard
+          image={ThoughtHarvesterImage}
+          title="ThoughtHarvester"
+          repoLink="https://github.com/arnald18/ThoughtHarvester"
+          deployedLink="https://secret-cliffs-31610-1cfd2b52332b.herokuapp.com/"
+        />
+        <PortfolioCard
+          image={ThoughtHarvesterImage}
+          title="ThoughtHarvester"
+          repoLink="https://github.com/arnald18/ThoughtHarvester"
+          deployedLink="https://secret-cliffs-31610-1cfd2b52332b.herokuapp.com/"
+        />
+        <PortfolioCard
+          image={ThoughtHarvesterImage}
+          title="ThoughtHarvester"
+          repoLink="https://github.com/arnald18/ThoughtHarvester"
+          deployedLink="https://secret-cliffs-31610-1cfd2b52332b.herokuapp.com/"
+        />
+      </div>
     </div>
   );
 }
